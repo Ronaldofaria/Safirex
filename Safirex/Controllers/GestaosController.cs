f8fb3429bee391a728f4d5fb22efdc6a2c7fd289
Ronaldo Faria <eco.ronaldo.faria@gmail.com>
2018-06-14 17:20:25 -0300

    atualização de versão
    Signed-off-by: Ronaldo Faria <eco.ronaldo.faria@gmail.com>

---
+++ Safirex/Controllers/GestaosController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Safirex.Models.ClassificacaoOrigem;
+
+namespace Safirex.Controllers
+{
+    public class GestaosController : BaseController
+    {
+
+        // GET: Governos
+        public ActionResult Index(int? page, string search = "")
+        {
+
+            ViewBag.search = search;
+            ViewBag.page = page;
+
+            int pageSize = 08;
+            int pageNumber = (page ?? 1);
+
+            var model = db.Gestaos.ToList();
+            if (search != "")
+            {
+                model = db.Gestaos.Where(prop => prop.Nome.Contains(search)).ToList();
+            }
+
+            return View(model.ToPagedList(pageNumber, pageSize));
+
+        }
+
+        // GET: Gestaos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gestao gestao = db.Gestaos.Find(id);
+            if (gestao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gestao);
+        }
+
+        // GET: Gestaos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gestaos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GestaoId,Nome")] Gestao gestao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gestaos.Add(gestao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gestao);
+        }
+
+        // GET: Gestaos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gestao gestao = db.Gestaos.Find(id);
+            if (gestao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gestao);
+        }
+
+        // POST: Gestaos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GestaoId,Nome")] Gestao gestao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gestao).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gestao);
+        }
+
+        // GET: Gestaos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gestao gestao = db.Gestaos.Find(id);
+            if (gestao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gestao);
+        }
+
+        // POST: Gestaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gestao gestao = db.Gestaos.Find(id);
+            db.Gestaos.Remove(gestao);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Safirex/Models/ClassificacaoEstrutura/Gestao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Safirex.Models.ClassificacaoOrigem
+{
+    public class Gestao
+    {
+        public int GestaoId { get; set; }
+        public string Nome { get; set; }
+    }
+}
